a79a43f0988598bb752c090cd34c9195ca4af71c
jesimar <jesimar.arantes@gmail.com>
2018-07-23 19:14:45 -0300

    Added code UAV-Plot-Mission

---
+++ UAV-Plot-Mission/exec-plot-mission.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author: Jesimar da Silva Arantes
+#Date: 23/07/2018
+#Last Update: 23/07/2018
+#Description: Script that run UAV-Plot-Mission system.
+#Descrição: Script que executa o sistema UAV-Plot-Mission.
+
+java -jar dist/UAV-Plot-Mission.jar
